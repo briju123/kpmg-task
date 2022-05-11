a71d27bd45737178da6e578ae763b9cb297a8e26
briju123 <30947115+briju123@users.noreply.github.com>
2022-05-11 16:47:32 +0530

    Update cpudiskusage.sh

--- cpudiskusage.sh
+++ cpudiskusage.sh
@@ -1,15 +1,13 @@
 HOSTNAME=$(hostname)
-DATET=$(date "+%Y-%m-%d ")
-DISKUSAGE=$(df -P | column -t | awk '{print $5})
+DISKUSAGE=$(df -h})
 
 echo 'HostName, Date, Disk' >> /opt/usagereport
-echo "$HOSTNAME, $DATE, $DISKUSAGE" >> /opt/usagereport
+echo "$HOSTNAME,$DISKUSAGE" >> /opt/usagereport
 
 for i in 'cat /scripts/hostlist';
 do
 	RHOSTNAME=$ (ssh $i hostname)
-	RDATE=$ (ssh $i 'date "+%Y-%m-%d"')
-	RDISKUSAGE=$ (ssh $i df -P | column -t | awk '{print $5})
-echo "$RHOSTNAME, $RDATE, $RDISKUSAGE"
+	RDISKUSAGE=$ (ssh $i df -h})
+echo "$RHOSTNAME, $RDISKUSAGE"
 done
 
